36f6380e3079d31af0a6c566058638e639405bd5
Aditya Prabowo <adityaprabowo2001@gmail.com>
2023-12-17 10:08:05 +0700

    fix route and prediction.py

--- app/app.py
+++ app/app.py
@@ -1,63 +1,42 @@
-from flask import Flask, request, jsonify, Blueprint
-from .model.prediction import predict, get_scholarship_details
-from google.cloud import storage
-import pandas as pd
-import joblib
+import sys
+import os
+from flask import Flask, request, jsonify
+from prediction import predict, get_scholarship_details, load_model_and_data
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 app = Flask(__name__)
 
-# Load resources using a Blueprint
-resources_bp = Blueprint('resources', __name__)
+MODEL, DATA_CONTENT_BASED_FILTERING = load_model_and_data()
 
-@resources_bp.record_once
-def on_load(state):
-    bucket_name = 'adityaschero'
-    vectorizer_file_name = 'tfidf_vectorizer.pkl'
-    model_file_name = 'tfidf_matrix.pkl'
-    data_file_name = 'beasiswa.csv'
-
-    download_file_from_gcs(bucket_name, f'model/{vectorizer_file_name}', f'app/model/{vectorizer_file_name}')
-    download_file_from_gcs(bucket_name, f'model/{model_file_name}', f'app/model/{model_file_name}')
-    download_file_from_gcs(bucket_name, f'data/{data_file_name}', f'app/data/{data_file_name}')
-
-    state.data_content_based_filtering = pd.read_csv(f'app/data/{data_file_name}')
-    state.model_file = f'app/model/{vectorizer_file_name}'
-    state.tfidf_vectorizer = joblib.load(state.model_file)
-
-def download_file_from_gcs(bucket_name, source_blob_name, destination_file_name):
-    storage_client = storage.Client()
-    bucket = storage_client.bucket(bucket_name)
-    blob = bucket.blob(source_blob_name)
-    blob.download_to_filename(destination_file_name)
-
-app.register_blueprint(resources_bp, url_prefix='/resources')
+BUCKET_NAME = 'zshdityaschero' 
 
 @app.route('/predict', methods=['POST'])
 def predict_scholarships():
     try:
         input_data = request.json
         education = input_data.get('jenjang pendidikan', '')
-        funding_type = input_data.get('pendanaan', '')
+        funding_type = input_data.get('pendanaan', '') 
         continent = input_data.get('benua', '')
 
-        recommendations = predict(education, funding_type, continent, app.data_content_based_filtering)
+        recommendations = predict(education, funding_type, continent, DATA_CONTENT_BASED_FILTERING)
 
         result = {"recommendations": recommendations}
         return jsonify(result)
 
     except Exception as e:
-        return jsonify({"error": str(e)})
+        return jsonify({"error": str(e)}), 500  
 
 @app.route('/scholarship_details', methods=['GET'])
 def get_details():
     try:
         scholarship_name = request.args.get('scholarship_name', '')
-        details = get_scholarship_details(scholarship_name, app.data_content_based_filtering)
+        details = get_scholarship_details(scholarship_name, DATA_CONTENT_BASED_FILTERING)
         return jsonify(details)
 
     except Exception as e:
-        return jsonify({"error": str(e)})
+        return jsonify({"error": str(e)}), 500 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)  # to run on Google Cloud Platform
-    # app.run(port=5000, debug=True)  # to run on local
+    app.run(host='0.0.0.0', port=8080, debug=True) # run on GCP
+    # app.run(port=5000, debug=True) # run on local
